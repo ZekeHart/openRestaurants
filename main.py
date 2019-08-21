5580fa1f145aa1a5a8c0c591d943358cdb9cd848
Zeke Hart <heirloch@gmail.com>
2019-08-21 14:30:58 -0400

    adds datetime module and adds the current datetime as a test argument for the function, prints out to verify that it is working and to check the format

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+import csv
+from datetime import datetime, time
+import time
+
+dt_now = datetime.now()
+
 def find_open_restaurants(csv_filename, search_datetime):
+    search_weekday_int = search_datetime.weekday()
+    search_time = search_datetime.time()
+
+    
+    restaurant_info_dict = {}
     with open (csv_filename) as file:
-        readfile = file.read()
-    print(readfile)
+        readfile = csv.DictReader(file)
+        for line in readfile:
+            restaurant_info_dict[line['Restaurant Name']] = line['Hours']
+            
+    # for pair in restaurant_info_dict:
+    print(search_time)
+    print(search_weekday_int)
+    # print(restaurant_info_dict['Yard House'])
+    # for line in readfile:
+    #     restaurant_info_dict
 
 
-find_open_restaurants('restaurants.csv', 0)
+find_open_restaurants('restaurants.csv', dt_now)
