a6bc47dfca345abb94ad03cf651b2d077bfbb146
Zeke Hart <heirloch@gmail.com>
2019-08-25 20:52:55 -0400

    test day check function. Not fully working but solves a problem with certain day formats

--- differentDayChecker.py
+++ differentDayChecker.py
@@ -9,7 +9,7 @@ def find_open_restaurants(csv_filename, search_datetime):
     reads in the info from the csv into a dictionary, then calls functions to see which
     restaurants are open on that day and time
     """
-    search_weekday = search_datetime.strftime("%a")
+    search_weekday = search_datetime.weekday()
     search_time = search_datetime.time()
     restaurant_info_dict = {}
     with open (csv_filename) as file:
@@ -17,8 +17,8 @@ def find_open_restaurants(csv_filename, search_datetime):
         for line in readfile:
             restaurant_info_dict[line['Restaurant Name']] = re.sub(' |day', '', line['Hours'])
     open_on_day = check_day(search_weekday, restaurant_info_dict)
-    open_restaurants = check_time(search_time, open_on_day)
-    print(open_restaurants)
+    # open_restaurants = check_time(search_time, open_on_day)
+    # print(open_restaurants)
             
 
 def check_day(day, restaurant_dict):
@@ -30,31 +30,39 @@ def check_day(day, restaurant_dict):
     """
     open_on_day = {}
     for restaurant in restaurant_dict:
-        if "Mon-Sun" in restaurant_dict[restaurant]:
-            open_on_day[restaurant] = restaurant_dict[restaurant]
-        else:
-            if '/' in restaurant_dict[restaurant]:
-                split_hours = restaurant_dict[restaurant].split('/')
-            else:
-                split_hours = restaurant_dict[restaurant]
+        if '/' in restaurant_dict[restaurant]:
+            split_hours = restaurant_dict[restaurant].split('/')
             for chunk in split_hours:
-                if day in chunk:
-                    open_on_day[restaurant] = chunk
-                elif day == 'Tue':
-                    if ('Mon-' in chunk) or ('-Wed' in chunk) or ('Sun-Thu' in chunk) or ('Sun-Fri' in chunk):
-                        open_on_day[restaurant] = chunk
-                elif day == 'Wed':
-                    if ('Tue-' in chunk) or ('-Thu' in chunk) or ('Mon-Fri' in chunk) or ('Mon-Sat' in chunk):
-                        open_on_day[restaurant] = chunk
-                elif day == 'Thu':
-                    if ('Wed-' in chunk) or ('-Fri' in chunk) or ('Mon-Sat' in chunk) or ('Tue-Sat' in chunk) or ('Tue-Sun' in chunk):
-                        open_on_day[restaurant] = chunk
-                elif day == 'Fri':
-                    if ('Thu-' in chunk) or ('-Sat' in chunk) or ('Tue-Sun' in chunk) or ('Wed-Sun' in chunk):
-                        open_on_day[restaurant] = chunk
-                elif day =="Sat":
-                    if ('-Sun' in chunk):
-                        open_on_day[restaurant] = chunk
+                first_time_index = re.search('\d', chunk)
+                time_string = chunk[first_time_index.start():]
+                day_string = chunk[:first_time_index.start()]
+                to_check = replaceWeekdays(day_string)
+        else:
+            split_hours = restaurant_dict[restaurant]
+            first_time_index = re.search('\d', split_hours)
+            time_string = split_hours[first_time_index.start():]
+            day_string = split_hours[:first_time_index.start()]
+            to_check = replaceWeekdays(day_string)
+        
+        print(restaurant)
+        print(time_string)
+        print(day_string)
+
+            #     elif day == 'Tue':
+            #         if ('Mon-' in chunk) or ('-Wed' in chunk) or ('Sun-Thu' in chunk) or ('Sun-Fri' in chunk):
+            #             open_on_day[restaurant] = chunk
+            #     elif day == 'Wed':
+            #         if ('Tue-' in chunk) or ('-Thu' in chunk) or ('Mon-Fri' in chunk) or ('Mon-Sat' in chunk):
+            #             open_on_day[restaurant] = chunk
+            #     elif day == 'Thu':
+            #         if ('Wed-' in chunk) or ('-Fri' in chunk) or ('Mon-Sat' in chunk) or ('Tue-Sat' in chunk) or ('Tue-Sun' in chunk):
+            #             open_on_day[restaurant] = chunk
+            #     elif day == 'Fri':
+            #         if ('Thu-' in chunk) or ('-Sat' in chunk) or ('Tue-Sun' in chunk) or ('Wed-Sun' in chunk):
+            #             open_on_day[restaurant] = chunk
+            #     elif day =="Sat":
+            #         if ('-Sun' in chunk):
+            #             open_on_day[restaurant] = chunk
     return open_on_day
 
 
@@ -88,6 +96,15 @@ def check_time(time_of_day, restaurant_info_dict):
             open_restaurants.append(restaurant)
     return open_restaurants
 
+def replaceWeekdays(day_string):
+    replaceDict = {
+        "Sun":0,
+        "Mon":1,
+        "Tue":2,
+        "Wed":3,
+        "Thu":4,
+        "Fri":5,
+    }
 
 if __name__ == "__main__":
     dt_now = datetime.now()
